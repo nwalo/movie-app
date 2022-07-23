35492ddb324c596b8ec10cbdefbf3372302e675d
Nwalo <nwalookechukwu@gmail.com>
2022-07-23 10:50:52 +0100

    load dynamic movies

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,32 @@
 import { React, useEffect, useState } from 'react'
-import { Box, Flex, Text, Input, HStack, Stack } from '@chakra-ui/react'
+import {
+  Box,
+  Flex,
+  Text,
+  Heading,
+  Input,
+  Center,
+  Stack,
+} from '@chakra-ui/react'
 import axios from 'axios'
 import cors from 'cors'
 
 const Main = () => {
-  const [load, setLoad] = useState([])
+  const [load, setLoad] = useState()
+  const [movie, setMovie] = useState('')
+  const [movieName, setMovieName] = useState('Sonic')
 
   useEffect(() => {
     async function fetchData() {
       const req = await axios.get(
-        'https://www.omdbapi.com/?s=sonic&apikey=69f5cb07',
+        `https://www.omdbapi.com/?s=${movieName}&apikey=69f5cb07`,
         cors(),
       )
       setLoad(req.data.Search)
     }
 
     fetchData()
-  }, [])
+  }, [movieName])
 
   const Movies = (props) => {
     // console.log(props)
@@ -42,14 +52,43 @@ const Main = () => {
     )
   }
 
+  let loadMovie = (
+    <Heading as="h4" fontSize="24px" mt="2em">
+      Unable to load resources, please check your connection ...
+    </Heading>
+  )
+
+  if (load) {
+    loadMovie = load.map((p, i) => {
+      return <Movies img={p.Poster} title={p.Title} key={i} />
+    })
+  }
+
+  const handleChange = (e) => {
+    setMovie(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(movie)
+    setMovieName(movie)
+    setMovie('')
+  }
+
   return (
     <Box bg="white" pl="77px" pt="98px" pr="57px">
       <Box pb="48px">
         <Text fontSize="24px" pb="4px">
           Search
         </Text>
-        <form>
-          <Input placeholder="Search movie title..." />
+        <form onSubmit={handleSubmit}>
+          <Input
+            placeholder="Search movie title..."
+            type="search"
+            onChange={handleChange}
+            value={movie}
+          />
         </form>
       </Box>
       <Box pb="67px">
@@ -58,11 +97,7 @@ const Main = () => {
             <Text fontSize="24px" pb="4px">
               Movie Category Name
             </Text>
-            <Flex wrap="wrap">
-              {load.map((p, i) => {
-                return <Movies img={p.Poster} title={p.Title} key={i} />
-              })}
-            </Flex>
+            <Flex wrap="wrap">{loadMovie}</Flex>
           </Box>
         </Stack>
       </Box>
